74be1e291502f36c0f48fef5240a109b0c19ccf7
sana <sananafari@yahoo.com>
2024-09-14 00:27:24 +0330

    FAQ

---
+++ JS/main.js
@@ -0,0 +1,8 @@
+//FAQ Accordin
+document.addEventListener('DOMContentLoaded', () => {
+    const faqContainer = document.querySelector ('.faq-content');
+
+    faqContainer.addEventListener('click', () => {
+     alert(1);
+    })
+});
